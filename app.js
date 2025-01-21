18c6b50e820c0f9474d6ac0a9cd4767e8c56e786
PRISCILA ALVES <75861909+priscilaalvessilva@users.noreply.github.com>
2025-01-21 07:47:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Bem-vindo(a) ao jogo de adivinha o número!");
+let numeroSecreto = 29;
+let chute = prompt("Digite um número entre 01 e 30");
+
+
+if (chute == numeroSecreto) {
+    console.log("Parabéns, você acertou!");
+}
+
+   
